3f4b845eb0a7927ffa234b441ad036f3a118a166
lblblong <1162275271@qq.com>
2018-09-19 16:00:25 +0800

    eidt about

--- src/renderer/components/widget/about/index.vue
+++ src/renderer/components/widget/about/index.vue
@@ -3,9 +3,8 @@
         <h4>ZRTools</h4>
         <p>版本号：v1.2.0</p>
         <h4>开发者</h4>
-        <p>公司：智汇机器人</p>
-        <p>维护联系人：李北龙</p>
-        <p>联系方式：libeilong@ismart-bot.com</p>
+        <p>李北龙</p>
+        <p>联系方式：1162275271@qq.com</p>
         <p>手机版ZTools：http://fir.im/ztool</p>
     </div>
 </template>
